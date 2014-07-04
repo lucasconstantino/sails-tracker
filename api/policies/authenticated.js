06c8158f7159aa253a7cedcd1a67d2476adbf1d7
Lucas Constantino <lucas@taller.net.br>
2014-07-03 23:30:21 -0300

    Created a authenticated policy.

---
+++ api/policies/authenticated.js
@@ -0,0 +1,11 @@
+/**
+ * authenticated
+ *
+ * @module      :: Policy
+ * @description :: Passport authentication police.
+ *
+ */
+
+module.exports = function(req, res, next) {
+  return req.isAuthenticated() ? next() : res.forbidden('not authenticated');
+};
