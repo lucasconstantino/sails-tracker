26f60d94fc29000636ce7e0425f2f63151a719af
Lucas Constantino <lucas@taller.net.br>
2014-07-03 22:59:08 -0300

    Configured simple routes for authentication.

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,11 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
-  }
+  },
+
+  // Authentication routes.
+  '/login': 'AuthController.login',
+  '/logout': 'AuthController.logout'
 
   /*
   // But what if you want your home page to display
