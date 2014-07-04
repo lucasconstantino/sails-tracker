ba3d00b78e19ecc66ff846296614940e0a3865b0
Lucas Constantino <lucas@taller.net.br>
2014-07-03 22:55:45 -0300

    Created basic authentication controller.

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,60 @@
+/**
+ * AuthController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+var passport = require('passport');
+
+module.exports = {
+
+  /**
+   * Perform login.
+   */
+  login: function (req, res) {
+
+    /**
+     * Handles any error during login attempt.
+     */
+    function loginError() {
+      return res.send(401, 'authentication failed');
+    }
+
+    // Attempt to login using local strategy.
+    // @todo: this could be enhanced to allow other kinds of authentication.
+    passport.authenticate('local', function (err, user, info) {
+      if (err || !user) return loginError();
+      req.login(user, function (err) {
+        if (err) return loginError();
+        return res.send(200, 'login successfully');
+      });
+    });
+  },
+
+  /**
+   * Perform logout.
+   */
+  logout = function (req, res) {
+    req.logout();
+    res.send(200, 'logout successfully')
+  },
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to AuthController)
+   */
+  _config: {}
+
+  
+};
