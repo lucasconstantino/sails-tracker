4e5a5efff8dafa874511d6c40d781ca3dc3c884b
Lucas Constantino <lucas@taller.net.br>
2014-07-04 02:36:40 -0300

    Moved passport logic to service.

---
+++ api/services/passport.js
@@ -0,0 +1,55 @@
+/**
+ * Passport
+ *
+ * Passport service definition.
+ */
+
+var passport      = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    bcrypt        = require('bcrypt'),
+    sails         = require('sails');
+
+// Passport session setup.
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session. Typically,
+// this will be as simple as storing the user ID when serializing, and finding
+// the user by ID when deserializing.
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findOne(id, done);
+});
+
+/**
+ * Default local strategy.
+ */
+function localStrategy(email, password, done) {
+  User.findOne({
+    email: email
+  }).done(function (err, user) {
+    if (err) return done(err);
+    if (!user) return done(null, false, {
+      message: 'unkown user with e-mail "' + email + '"'
+    });
+
+    bcrypt.compare(password, user.password, function (err, res) {
+      if (err) return done(err);
+      if (!res) return done(null, false, {
+        message: 'wrong password for user "' + email + '"'
+      });
+
+      return done(null, user);
+    });
+  });
+}
+
+// Use the LocalStrategy within Passport.
+// Strategies in passport require a `verify` function, which accept
+// credentials (in this case, a username and password), and invoke a callback
+// with a user object.
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, localStrategy));
--- config/passport.js
+++ config/passport.js
@@ -4,51 +4,15 @@
  * Passport integration for Sails.js.
  */
 
-var passport      = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    bcrypt        = require('bcrypt');
-
-// Configure user serialization method.
-passport.serializeUser(function (user, done) {
-  done(null, user[0].id);
-});
-
-// Configure user deserialization method.
-passport.deserializeUser(function (id, done) {
-  User.findById(id, done);
-});
-
-/**
- * Default local strategy.
- */
-function localStrategy(username, password, done) {
-  User.findByUsername(username).done(function (err, user) {
-    if (err) return done(err);
-    if (!user || !user.length) return done(null, false, {
-      message: 'User "' + username + '" not found.'
-    });
-
-    bcrypt.compare(password, user[0].password, function (err, res) {
-      if (!err) return done(err);
-      if (!res) return done(null, false, {
-        message: 'Invalid password for user "' + username + '".'
-      });
-
-      return done(null, user);
-    });
-  });
-}
-
-// Configure passport to make use of a custom local strategy.
-passport.use(new LocalStrategy(localStrategy));
+var passport = require('passport'),
+    sails    = require('sails');
 
 module.exports = {
   express: {
     customMiddleware: function (app) {
-      // @todo: user debug, instead of console.log?
-      console.log('express midleware for passport');
       app.use(passport.initialize());
       app.use(passport.session());
+      sails.log.debug('express midleware for passport');
     }
   }
 };
