18cfcb4a818caa74c26349e676eb47bf08cb1473
Lucas Constantino <lucas@taller.net.br>
2014-07-06 00:48:59 -0300

    Configured policies.

--- config/policies.js
+++ config/policies.js
@@ -16,34 +16,22 @@
 
 module.exports.policies = {
 
-  // Default policy for all controllers and actions
-  // (`true` allows public access)
-  '*': 'authenticated',
+  '*': 'isAuthenticated',
 
-  // Allow public access to new user registering.
+  // Allow anonymous to new user registering.
   UserController: {
-   'create': true
+    '*': ['isAuthenticated', 'self'],
+    'find': true,
+    'findOne': true,
+    'create': true
   },
 
   // Allow public access to all authentication methods.
   AuthController: {
    '*': true
+  },
+
+  OrganizationController: {
+    '*': ['isAuthenticated', 'isOwner']
   }
-
-	// Here's an example of mapping some policies to run before
-  // a controller and its actions
-	// RabbitController: {
-
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		// '*': false,
-
-		// For the action `nurture`, apply the 'isRabbitMother' policy
-		// (this overrides `false` above)
-		// nurture	: 'isRabbitMother',
-
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		// feed : ['isNiceToAnimals', 'hasRabbitFood']
-	// }
 };
